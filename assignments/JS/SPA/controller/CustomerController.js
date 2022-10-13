bb743719716def663ba7513e13b528e12a1a04ab
sajithmadusamkha <sajithmadusankha02@gmail.com>
2022-10-13 06:28:11 +0530

    content updated

--- assignments/JS/SPA/controller/CustomerController.js
+++ assignments/JS/SPA/controller/CustomerController.js
@@ -4,11 +4,39 @@ $("#saveCustomer").click(function () {
     let customerId = $("#cusId").val();
     let customerName = $("#cusName").val();
     let customerAddress = $("#address").val();
-    let customerEmail = $("#email").val();
+    let customerSalary = $("#salary").val();
 
-    let customerObject = Customer(customerId,customerName,customerAddress,customerEmail);
+    let customerObject = Customer(customerId,customerName,customerSalary,customerAddress);
 
     customers.push(customerObject);
-
+    loadAllCustomers();
+    bindCusRowClick();
 });
+
+function loadAllCustomers() {
+    $("#tblCustomer").empty();
+
+    for (var customer of customers) {
+        console.log(customer);
+
+        var row = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.salary}</td><td>${customer.address}</td></tr>`
+
+        $("#tblCustomer").append(row);
+    }
+}
+
+function bindCusRowClick() {
+    $("#tblCustomer>tr").click(function () {
+       let id = $(this).children(":eq(0)").text();
+       let name = $(this).children(":eq(1)").text();
+       let salary = $(this).children(":eq(2)").text();
+       let address = $(this).children(":eq(3)").text();
+
+        $('#update-cusId').val(id);
+        $('#update-cusName').val(name);
+        $('#update-salary').val(salary);
+        $('#update-address').val(address);
+    });
+}
+
 
