89227c93b9b05e9b57cec05a41cee111c207600d
sajithmadusamkha <sajithmadusankha02@gmail.com>
2022-10-14 09:47:01 +0530

    content updated

--- assignments/JS/SPA/controller/CustomerController.js
+++ assignments/JS/SPA/controller/CustomerController.js
@@ -12,6 +12,7 @@ $("#saveCustomer").click(function () {
     loadAllCustomers();
     bindCusRowClick();
     loadAllCustomerForOption();
+    clearCusTexts();
 });
 
 function loadAllCustomers() {
@@ -139,8 +140,110 @@ function setCusTextField(id,name,salary,address){
 
 /****** Customer regular expressions ******/
 const regExId = /^(C00-)[0-9]{1,3}$/;
-const regCusName = /^[A-z ]{5,20}$/;
+const regCusName = /^[A-z ]{4,20}$/;
 const regExSalary = /^\d{0,9}(\.\d{1,4})?$/;
 const regExAddress = /^[0-9/A-z. ,]{5,}$/
 
 let customerValidations = [];
+customerValidations.push({reg:regExId, field: $('#cusId'),error:'Item Code Pattern Is Wrong : C00-001'});
+customerValidations.push({reg:regCusName, field: $('#cusName'),error:'Name Pattern Is Wrong : A-z'});
+customerValidations.push({reg:regExSalary, field: $('#salary'),error:'Salary Pattern Is Wrong : 2000 or 2000.00'});
+customerValidations.push({reg:regExAddress, field: $('#address'),error:'Address Pattern Is Wrong.'});
+
+$('#cusId,#cusName,#salary,#address').on('keydown',function (event) {
+   if (event.key == "Tab"){
+       event.preventDefault();
+   }
+});
+
+$('#cusId,#cusName,#salary,#address').on('keyup',function () {
+    customerValidity();
+});
+
+$('#cusId,#cusName,#salary,#address').on('blur',function () {
+    customerValidity();
+});
+
+$('#cusId').on('keydown', function (event) {
+   if(event.key == "Enter" && checkCustomer(regExId,$('#cusId'))) {
+       $('#cusName').focus();
+   }  else {
+       $('#cusId').focus();
+   }
+});
+
+$('#cusName').on('keydown', function (event) {
+    if (event.key == "Enter" && check(regExItemName, $('#cusName'))) {
+        $('#salary').focus();
+    }
+});
+
+$('#salary').on('keydown', function (event) {
+    if (event.key == "Enter" && check(regExItemName, $('#salary'))) {
+        $('#address').focus();
+    }
+});
+
+$('#address').on('keydown', function (event) {
+    if (event.key == "Enter" && check(regExItemQty, $('#address'))) {
+        let res = confirm("Do you want to add this customer.?");
+        if(res) {
+            clearCusTexts();
+        }
+    }
+});
+
+function customerValidity(){
+    let errCount = 0;
+    for (let validation of customerValidations) {
+        if(checkCustomer(validation.reg,validation.field)) {
+            inputCusSuccess(validation.field,"");
+        } else {
+            errCount += 1;
+            inputCusError(validation.field,validation.error);
+        }
+    }
+    setCusBtnState(errCount);
+}
+
+function checkCustomer(regex,textField){
+    let inputValue = textField.val();
+    return regex.test(inputValue) ? true : false;
+}
+
+function inputCusSuccess(textField,error) {
+    if(textField.val().length <= 0){
+        defaultCusText(textField,"");
+    } else {
+        textField.css('border','2px solid green');
+        textField.parent().children('span').text(error);
+    }
+}
+
+function inputCusError(textField,error) {
+    if (textField.val().length <= 0) {
+        defaultCusText(textField,"");
+    } else {
+        textField.css('border','2px solid red');
+        textField.parent().children('span').text(error);
+    }
+}
+
+function defaultCusText(textField,error){
+    textField.css("border","1px solid #ced4da");
+    textField.parent().children('span').text(error);
+}
+
+function setCusBtnState(val){
+    if (val > 0) {
+        $("#saveCustomer").attr('disabled', true);
+    } else {
+        $("#saveCustomer").attr('disabled', false);
+    }
+}
+
+function clearCusTexts(){
+    $('#cusId').focus();
+    $('#cusId,#cusName,#salary,#address').val("");
+    customerValidity();
+}
